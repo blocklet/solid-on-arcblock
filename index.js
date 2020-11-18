781de0a515b95a658625ae403f120835197b5ab1
wangshijun <wangshijun2010@gmail.com>
2020-11-18 15:07:05 +0800

    chore: polish index.js and npm scripts

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const solid = require('solid-server')
+const solid = require('solid-server');
 
-// Startin solid server
-const ldp = solid.createServer()
-const port = process.env.PORT || process.env.BLOCKLET_PORT || 8000
+// Starting solid server
+const ldp = solid.createServer();
+const port = process.env.PORT || process.env.BLOCKLET_PORT || 8000;
 ldp.listen(port, function () {
-  console.log('Starting Blocklet on port ' + port)
-  console.log('Solid Server run on /')
-})
+  console.log('Starting Blocklet on port ' + port);
+  console.log('Solid Server run on /');
+});
